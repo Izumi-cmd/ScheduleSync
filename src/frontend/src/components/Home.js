da84090f667fe0fe351a85d72646ad9432adfb53
Izumi_cmd <ryutaro.jp@gmail.com>
2024-10-03 01:41:07 +0900

    :fix:classNameの修正とコンテンツの共通化を設定

--- src/frontend/src/components/Home.js
+++ src/frontend/src/components/Home.js
@@ -1,39 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/components/button.css";
 
 export const Home = () => {
   return (
     <div className="home container">
-      <div className="home__content-head">
-        <h2 className="home__content-head-title">スケジュールを調整しましょう。</h2>
-        <div className="home__content-lead">
-          <p className="home__content-lead-text">
-            ScheduleSyncは、イベントのスケジュールを調整するためのサービスです。
-            <br />
-            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-          </p>
-        </div>
-      </div>
-      <div className="home__content-body">
+      <h2 className="home__title container__title">スケジュールを調整しましょう。</h2>
+      <p className="home__lead container__lead">
+        ScheduleSyncは、イベントのスケジュールを調整するためのサービスです。
+        <br />
+        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+      </p>
+      <div className="home__actions container__actions">
         <div className="home__contents">
           <p className="home__description">
-            イベントを作成して、
-            <br />
-            参加者のスケジュールを調整しましょう。
+            イベントを作成して、参加者のスケジュールを調整しましょう。
           </p>
-          <div className="home__buttons">
-            <Link to="/create-event" className="btn create-event__button">
-              イベントを作成する
-            </Link>
-          </div>
+          <Link to="/create-event" className="btn home__create-button">
+            イベントを作成する
+          </Link>
         </div>
         <div className="home__contents">
           <p className="home__description">以下のボタンから調整に参加しましょう。</p>
-          <div className="home__buttons">
-            <Link to="/join-event" className="btn join-event__button">
-              イベントに参加する
-            </Link>
-          </div>
+          <Link to="/join-event" className="btn home__join-button">
+            イベントに参加する
+          </Link>
         </div>
       </div>
     </div>
